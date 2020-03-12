041bf28a5e8f0604130cc1d436787ca713285f5a
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-03-12 13:31:44 -0600

    Video 211  + Enviar y renderizar mensajes

--- public/js/socket-chat-jquery.js
+++ public/js/socket-chat-jquery.js
@@ -1,7 +1,13 @@
 var params = new URLSearchParams( window.location.search );
 
+var nombre = params.get('nombre');
+var sala = params.get('sala');
+
 // Referencias de jQuery
 var divUsuarios = $('#divUsuarios');
+var formEnviar = $('#formEnviar');
+var txtMensaje = $('#txtMensaje');
+var divChatbox = $('#divChatbox');
 
 // Funciones para renderizar usuarios
 function renderizarUsuarios ( personas ) { // [{}, {}, {}]
@@ -24,14 +30,48 @@ function renderizarUsuarios ( personas ) { // [{}, {}, {}]
     divUsuarios.html( html );
 }
 
+function renderizarMensajes( mensaje ) {
+
+    let html = '';
+
+    html += '<li class="animated fadeIn">';
+    html += '    <div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" /></div>';
+    html += '    <div class="chat-content">';
+    html += '        <h5>'+mensaje.nombre+'</h5>';
+    html += '        <div class="box bg-light-info">'+mensaje.mensaje+'</div>';
+    html += '    </div>';
+    html += '    <div class="chat-time">10:56 am</div>';
+    html += '</li>';
+
+    divChatbox.append(html);
+
+}
+
 // Listeners
 divUsuarios.on('click', 'a', function() {
 
     var id = $(this).data('id');
 
     if ( id ) {
-
         console.log(id);
     }
+});
+
+formEnviar.on('submit', function(e) {
+
+    e.preventDefault();
+
+    if ( txtMensaje.val().trim().length === 0 ) {
+        return;
+    }
+
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function(mensaje) {
+        txtMensaje.val('').focus();
+        renderizarMensajes(mensaje);
+    });
+
 
 });
